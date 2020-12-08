1fcd20622aeabdb1bc5ad8be10dc0d9400ff9206
tone94 <tone94@sina.cn>
2020-12-08 16:56:44 +0800

    lodash

--- miao/lodash/tone94-lodash.js
+++ miao/lodash/tone94-lodash.js
@@ -10,8 +10,40 @@ var tone94 = function () {
     return result
   }
 
+  function join(ary, sep) {
+    var result = ""
+    if (sep == null) sep = ","
+    for (var i = 0; i < ary.length - 1; i++) {
+      result += ary[i] + sep
+    }
+    result += ary[ary.length - 1]
+    return result
+  }
+
+  function last(ary) {
+    if (!ary) return undefined
+    return ary[ary.length - 1]
+  }
+
+  function lastIndexOf(ary, val, fromIndex = ary.length - 1) {
+    if (val == null) return ary.length
+    var idx = -1
+    // todo 需要处理fromIndex
+    for (var i = fromIndex; i >= 0; i--) {
+      if (ary[i] == val) {
+        idx = i
+        break
+      }
+    }
+    return idx
+  }
+
+  // 返回一个对象
   return {
     compact,
+    join,
+    last,
+    lastIndexOf,
   }
 
 }()

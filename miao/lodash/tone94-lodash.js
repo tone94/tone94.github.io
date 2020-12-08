25985b2c0d66edaee8ceb5e3fa36e3350785d3b0
tone94 <tone94@sina.cn>
2020-12-08 20:10:42 +0800

    chunk

--- miao/lodash/tone94-lodash.js
+++ miao/lodash/tone94-lodash.js
@@ -38,6 +38,25 @@ var tone94 = function () {
     return idx
   }
 
+  function chunk(ary, size = 1) {
+    var result = []
+    var tail = ary.length - (ary.length % size)
+    for (var i = 0; i < tail;) {
+      var p = []
+      for (var j = 0; j < size; j++) {
+        p.push(ary[i++])
+      }
+      result.push(p)
+    }
+    var p = []
+    while (tail < ary.length) {
+      p.push(ary[tail])
+      tail++
+    }
+    if (p.length) result.push(p)
+    return result
+  }
+
   // 返回一个对象
   return {
     compact,
